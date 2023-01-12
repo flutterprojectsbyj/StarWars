139a4403ea3910941182494e059f2a0ff2e212a8
jmpfbmx <jmpf.bmx@gmail.com>
2023-01-12 21:18:15 +0100

    star-wars-characters-jperfor849: Sort up the project
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-import 'StarWars.dart';
+import 'database/StarWars.dart';
 
 void main() {
   runApp(const MaterialApp(
